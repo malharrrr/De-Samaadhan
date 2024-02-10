d8bf5476bf6880c09ac90b6ce370269b024c1e21
Thanatos1204 <bhargavpandit01@gmail.com>
2024-02-10 18:41:16 +0530

    dashboard-cards-resolved

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,40 @@
-import { cards } from "../lib/data";
 import Card from "../ui/dashboard/card/card";
 import Chart from "../ui/dashboard/chart/chart";
 import styles from "../ui/dashboard/dashboard.module.css";
 import Transactions from "../ui/dashboard/transactions/transactions";
 
 const Dashboard = () => {
+
+  // DUMMY DATA
+
+ const cards = [
+  {
+    id: 1,
+    title: "Total Complaints",
+    number: 10.928,
+    change: 12,
+  },
+  {
+    id: 2,
+    title: "Pending",
+    number: 8.236,
+    change: -2,
+  },
+  {
+    id: 3,
+    title: "Approved",
+    number: 6.642,
+    change: 18,
+  },
+
+  {
+    id: 4,
+    title: "Flagged",
+    number: 6.642,
+    change: 13,
+  },
+];
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -2,6 +2,9 @@ import { MdSupervisedUserCircle } from "react-icons/md";
 import styles from "./card.module.css";
 
 const Card = ({ item }) => {
+
+
+  
   return (
     <div className={styles.container}>
       <MdSupervisedUserCircle size={24} />
